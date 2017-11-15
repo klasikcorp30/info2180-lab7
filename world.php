f7b1e0bbac40d3a0ce29e0a8d161c7f2e04fed4a
klasikcorp30 <32335856+klasikcorp30@users.noreply.github.com>
2017-11-15 17:36:14 -0400

    css style added and backgorund image.  php cde up date

--- world.php
+++ world.php
@@ -5,14 +5,26 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 
+
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries");
+if (!empty($_GET)){
+  if (array_key_exists("all", $_GET)){
+    $stmt = $conn -> query ("SELECT * FROM countries");
+  }
+  else if(array_key_exists("country", $_GET)){
+    $country = $_GET['country'];
+    $stmt = $conn->query('SELECT * FROM countries WHERE name = "%$country%"');
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    echo '<ul>';
+    foreach ($stmt as $row) {
+        {
+      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        }
+    
+    echo '</ul>';
+  }
 }
-echo '</ul>';s
+}
+
